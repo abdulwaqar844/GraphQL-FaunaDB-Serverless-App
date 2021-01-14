3efe28125122ddd329999f47effac416e67c7102
Abdul Waqar <abdulwaqar844@gmaill.com>
2021-01-15 01:22:11 +0500

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 
 const GET_TODOS = gql`
@@ -10,10 +10,66 @@ const GET_TODOS = gql`
   task
   status
 }} `
-console.log(res)
-export default function Home(){
+const ADD_TODO = gql`
+    mutation addTodo($task: String!){
+        addTodo(task: $task){
+            task
+        }
+    }
+`
+export default function Index() {
+  let inputText;
+
+  const [addTodo] = useMutation(ADD_TODO);
+  const addTask = () => {
+    addTodo({
+      variables: {
+        task: inputText.value
+      },
+      refetchQueries: [{ query: GET_TODOS }]
+    })
+    inputText.value = "";
+  }
+
   const { loading, error, data } = useQuery(GET_TODOS);
+
+  if (loading)
+    return <h2>Loading..</h2>
+
+  if (error) {
+    console.log(error)
+    return <h2>Error</h2>
+  }
+
   return (
-    <div>Hello World</div>
+    <div>
+      <label>
+        <h1> Add Task </h1>
+        <input type="text" ref={node => {
+          inputText = node;
+        }} />
+      </label>
+      <button onClick={addTask}>Add Task</button>
+
+      <br /> <br />
+      <table border="2" width="500px" >
+        <thead>
+          <th>Task</th>
+          <th>Status</th>
+        </thead>
+        <tbody>
+          {data.todos.map(d => {
+            return (
+              <tr key={d.id}>
+                <td>{d.task}</td>
+                <td>{d.status ? "Pending" : "Completed"}</td>
+              </tr>
+            )
+          }
+          )}
+        </tbody>
+      </table>
+
+    </div>
   )
 }
