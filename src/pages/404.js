edb05987382f8589d8cbb56e0facf801e6e38d27
Abdul Waqar <abdulwaqar844@gmaill.com>
2021-01-16 21:34:23 +0500

    updated error file

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,10 @@ import React from "react"
 const NotFoundPage = () => (
   <div>
       <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesnt exist... the sadness.</p>
   </div>
   
 )
 
 export default NotFoundPage
+               
